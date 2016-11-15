1892389b34ba7f5bf8c523c8110175c09f2dd8da
DellSoftware-SimonDu <simon.du@software.dell.com>
2016-11-15 11:45:44 +0800

    2
    2

--- PythonTest/city_functions.py
+++
@@ -1,7 +0,0 @@
-def get_city_country(city,country,population=''):
-	if population:
-		city_country = (city.title() + ',' + country.title() + ' - population ' + population)
-	else:
-		city_country = city.title() + ',' + country.title()
-	return city_country
-
